9ee92927e854adcd0382f947f190ef4ad6598c09
MahmoodAbuAwwad <abuawwadmahmood@gmail.com>
2020-11-04 09:47:32 +0200

    Update requests.service.ts

--- kubernetes/src/app/requests.service.ts
+++ kubernetes/src/app/requests.service.ts
@@ -2,25 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 @Injectable()
 export class ConfigService {
   constructor(private http: HttpClient) {}
 
-  ApiGetData = 'http://localhost:5000/api/v1/fetch';
-  ApiAddData = 'http://localhost:5000/api/v1/add/';
-
+  ApiGetData = '/api/v1/fetch';
+  ApiAddData = '/api/v1/add/';
+  HostAPI = environment.apiHost;
 
 
   // tslint:disable-next-line:typedef
   getData() {
-    return this.http.get(this.ApiGetData);
+    return this.http.get( this.HostAPI + this.ApiGetData);
   }
 
   // tslint:disable-next-line:typedef
   addData(text: string){
 
 
-    const url = this.ApiAddData + text;
+    const url = this.HostAPI + this.ApiAddData + text;
     return this.http.post(url , text, {responseType: 'text'}).subscribe();
   }
 }
