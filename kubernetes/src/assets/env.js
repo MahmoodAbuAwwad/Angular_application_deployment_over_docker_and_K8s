add98c18143dd12fc339c5b3331e9c11c9d078c3
MahmoodAbuAwwad <abuawwadmahmood@gmail.com>
2020-11-10 09:31:22 +0200

    Update env.js

--- kubernetes/src/assets/env.js
+++ kubernetes/src/assets/env.js
@@ -2,5 +2,6 @@
     window.env = window.env || {};
     // Environment variables
     window["env"]["API_URL"] = "192.168.204.226";
+    window["env"]["PORT"] = "5000";
     window["env"]["debug"] = "${DEBUG}";
   })(this);
