1cd2a76e113472850de1bdb3a299175c3361fd88
MahmoodAbuAwwad <abuawwadmahmood@gmail.com>
2020-11-10 09:30:46 +0200

    Update env.template.js

--- kubernetes/src/assets/env.template.js
+++ kubernetes/src/assets/env.template.js
@@ -2,5 +2,6 @@
     window.env = window.env || {};
     // Environment variables
     window["env"]["API_URL"] = "${API_URL}";
+    window["env"]["PORT"] = "${PORT}";
     window["env"]["debug"] = "${DEBUG}";
   })(this);
